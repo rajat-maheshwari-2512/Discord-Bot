ea60a33b828d85c39945dea5cc084056e103dcb1
Rajat Maheshwari <54249328+rajatmaheshwari2512@users.noreply.github.com>
2020-11-22 20:03:36 +0530

    Fixed Token

--- index.js
+++ index.js
@@ -1,79 +1,87 @@
-'use strict';
+"use strict";
 var author;
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('ready', () => {
-  console.log('I am ready!');
+client.on("ready", () => {
+  console.log("I am ready!");
 });
-client.on('message',message=>{
-  if (message.content==="!year")
-  {
-    author=message.author;
-    message.channel.send(`${author} Select the year you are currently studying in to get the respective role.
+client.on("message", (message) => {
+  if (message.content === "!year") {
+    author = message.author;
+    message.channel
+      .send(
+        `${author} Select the year you are currently studying in to get the respective role.
     **A**    First
     **B**    Second
     **C**    Third
     **D**    Fourth
-    **E**    Fifth`)
-    .then((message)=>{
-      message.react("🇦");
-      message.react("🇧");
-      message.react("🇨");
-      message.react("🇩");
-      message.react("🇪");
-      client.on("messageReactionAdd",(reaction,user)=>{
-        if(reaction.emoji.name==="🇦"&&user.id===author.id)
-        {
-          const guildMember = reaction.message.guild.members.cache.find(member=>member.id===user.id);
-          const role = message.guild.roles.cache.find(role=>role.name==="First Year");
-          guildMember.roles.add(role)
-          .then((resp)=>{
-            message.edit(`${author}, your role has been updated`);
-            message.reactions.removeAll();
-          });
-        }
-        else if(reaction.emoji.name==="🇧"&&user.id===author.id)
-        {
-          const guildMember = reaction.message.guild.members.cache.find(member=>member.id===user.id);
-          const role = message.guild.roles.cache.find(role=>role.name==="Second Year");
-          guildMember.roles.add(role)
-          .then((resp)=>{
-            message.edit(`${author}, your role has been updated`);
-            message.reactions.removeAll();
-          });
-        }
-        else if(reaction.emoji.name==="🇨"&&user.id===author.id)
-        {
-          const guildMember = reaction.message.guild.members.cache.find(member=>member.id===user.id);
-          const role = message.guild.roles.cache.find(role=>role.name==="Third Year");
-          guildMember.roles.add(role)
-          .then((resp)=>{
-            message.edit(`${author}, your role has been updated`);
-            message.reactions.removeAll();
-          });
-        }
-        else if(reaction.emoji.name==="🇩"&&user.id===author.id)
-        {
-          const guildMember = reaction.message.guild.members.cache.find(member=>member.id===user.id);
-          const role = message.guild.roles.cache.find(role=>role.name==="Fourth Year");
-          guildMember.roles.add(role)
-          .then((resp)=>{
-            message.edit(`${author}, your role has been updated`);
-            message.reactions.removeAll();
-          });
-        }
-        else if(reaction.emoji.name==="🇪"&&user.id===author.id)
-        {
-          const guildMember = reaction.message.guild.members.cache.find(member=>member.id===user.id);
-          const role = message.guild.roles.cache.find(role=>role.name==="Fifth Year");
-          guildMember.roles.add(role)
-          .then((resp)=>{
-            message.edit(`${author}, your role has been updated`);
-            message.reactions.removeAll();
-          });
-        }
-      })
-    })
+    **E**    Fifth`
+      )
+      .then((message) => {
+        message.react("🇦");
+        message.react("🇧");
+        message.react("🇨");
+        message.react("🇩");
+        message.react("🇪");
+        client.on("messageReactionAdd", (reaction, user) => {
+          if (reaction.emoji.name === "🇦" && user.id === author.id) {
+            const guildMember = reaction.message.guild.members.cache.find(
+              (member) => member.id === user.id
+            );
+            const role = message.guild.roles.cache.find(
+              (role) => role.name === "First Year"
+            );
+            guildMember.roles.add(role).then((resp) => {
+              message.edit(`${author}, your role has been updated`);
+              message.reactions.removeAll();
+            });
+          } else if (reaction.emoji.name === "🇧" && user.id === author.id) {
+            const guildMember = reaction.message.guild.members.cache.find(
+              (member) => member.id === user.id
+            );
+            const role = message.guild.roles.cache.find(
+              (role) => role.name === "Second Year"
+            );
+            guildMember.roles.add(role).then((resp) => {
+              message.edit(`${author}, your role has been updated`);
+              message.reactions.removeAll();
+            });
+          } else if (reaction.emoji.name === "🇨" && user.id === author.id) {
+            const guildMember = reaction.message.guild.members.cache.find(
+              (member) => member.id === user.id
+            );
+            const role = message.guild.roles.cache.find(
+              (role) => role.name === "Third Year"
+            );
+            guildMember.roles.add(role).then((resp) => {
+              message.edit(`${author}, your role has been updated`);
+              message.reactions.removeAll();
+            });
+          } else if (reaction.emoji.name === "🇩" && user.id === author.id) {
+            const guildMember = reaction.message.guild.members.cache.find(
+              (member) => member.id === user.id
+            );
+            const role = message.guild.roles.cache.find(
+              (role) => role.name === "Fourth Year"
+            );
+            guildMember.roles.add(role).then((resp) => {
+              message.edit(`${author}, your role has been updated`);
+              message.reactions.removeAll();
+            });
+          } else if (reaction.emoji.name === "🇪" && user.id === author.id) {
+            const guildMember = reaction.message.guild.members.cache.find(
+              (member) => member.id === user.id
+            );
+            const role = message.guild.roles.cache.find(
+              (role) => role.name === "Fifth Year"
+            );
+            guildMember.roles.add(role).then((resp) => {
+              message.edit(`${author}, your role has been updated`);
+              message.reactions.removeAll();
+            });
+          }
+        });
+      });
   }
 });
-client.login('NzU1NDU2NzEyMTA2MDQ5NTQ2.X2Dj4w._-iLtwL8BqR1-_M9-viPYCYY7j4');
+client.login("NzU1NDU2NzEyMTA2MDQ5NTQ2.X2Dj4w.X_NczpGNzA3fqWOsq9qtNisMa2I");
